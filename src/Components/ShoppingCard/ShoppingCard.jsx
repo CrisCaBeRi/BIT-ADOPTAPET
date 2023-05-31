a06f2211984a3f96db2ac59244ba73839e3cdfb2
Cami1107 <mpachecod@unal.edu.co>
2023-05-30 23:01:11 -0500

    feat: add somethings

---
+++ src/Components/ShoppingCard/ShoppingCard.jsx
@@ -0,0 +1,42 @@
+import {ShopingCard} from '../styles/ShopingCard.styles';
+
+const ShoppingCard = () => {
+    const productos =[
+        {
+            'id':'1',
+            'name':'ball',
+            'tittle':'bola',
+            'precio':2500,
+        },
+        {
+            'id':'2',
+            'name':'dogsfood',
+            'tittle':'comida de perro',
+            'precio':2500,
+        },
+        {
+            'id':'3',
+            'name':'bone',
+            'tittle':'hueso',
+            'precio':2500,
+        },
+        {
+            'id':'4',
+            'name':'catsfood',
+            'tittle':'comida de gato',
+            'precio':2500,
+        },
+        {
+            'id':'5',
+            'name':'carpet',
+            'tittle':'alfombra',
+            'precio':3500,
+        },
+    ];    
+    return (
+        <>
+        <CardContainer>
+        </CardContainer>
+        </>
+    );
+}
---
+++ src/Components/ShoppingCard/styles/ShoppingCard.Styles.jsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled"
+
+export const CardContainer = styled('div')(() => ({
+    backgroundColor: 'white',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+}));
+
