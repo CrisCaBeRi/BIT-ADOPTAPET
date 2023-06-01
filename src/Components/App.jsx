8ed4b0f32b6166e0d7a3ad4ae6b482d0ead14fb0
1999CDavid <carlosdavidjurado@gmail.com>
2023-06-01 00:20:14 -0500

    Creacion componente GeneralDescription y componente Dona

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,38 +1,8 @@
-
-
-
-
-
-/* import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ItemList from "../Components/ItemList/ItemList.jsx";
-import { ShopingCart } from "./ShoppingCart/ShopingCart"; */
-
-import AppUsers from "./Login/AppUsers";
-
-//import { Header } from "./Header/Header";
-
-
-//? 
-
+import GeneralDescription from "./GeneralDescription/GeneralDescription";
 const App = () => {
   return (
-
-    <AppUsers/>
-
-
-    
-
-
-
-    
+    <GeneralDescription/>
   );
 };
 export default App;
 
-{/* <Header/> */}
-/* <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ItemList />}/>
-        <Route path="/cart" element={<ShopingCart />} />       
-      </Routes>
-    </BrowserRouter> */
---
+++ src/Components/GeneralDescription/GeneralDescription.jsx
@@ -0,0 +1,24 @@
+import {dataPet} from '../Data/adopta'
+import "../GeneralDescription/GeneralDescriptionStyles.css"
+const GeneralDescription = () => {
+    return(<>
+    <section className="cards">
+        {dataPet.map((pet)=>(
+            <article className="petCard" key={`article-${pet.id}`}>
+                <header>
+                    <h1 className="title">{pet.name}</h1>
+                </header>
+                <img src={pet.imgUrl} alt="Pet Image"/>
+                <footer className="footerCard">
+                    <ul className="footerCard-li">
+                        <li>Raza: {pet.petBreed}</li>
+                        <li>Edad: {pet.petAge}</li>
+                        <li>Genero:{pet.gender}</li>
+                    </ul>
+                </footer>
+            </article>
+    ))}
+    </section>
+    </>)
+}
+export default GeneralDescription
