daf2334d048118fd36be7c486bebf5e5fc557cdb
1999CDavid <carlosdavidjurado@gmail.com>
2023-06-02 02:05:05 -0500

    agregando estilos component donate

--- src/Components/Donate/Donate.jsx
+++ src/Components/Donate/Donate.jsx
@@ -1,33 +1,36 @@
 
 import "../Donate/DonateStyles.css";
 const Donate = () => {
+    const sendwhatsappDonate = (dona) => {
+        let number = 573124116756;
+        let url = `https://wa.me/${number}?text= Hola, estoy interesado en adoptar a ${pet.name}`;
+        window.open(url);
+      };
     return(
     <>
-        <article className="CardDonate">
-            <section className="ContentDonate">
-                <header>
-                    <h1 className="title">Dona</h1>
-                </header>
+        <section className="CardDonate">
+            <span className="HeaderCardDonate" >
+                <h1 className="titleDonate">Donaciones</h1>
+            </span>
+            <article className="ContentDonate" >
                 <figure className="DonaFigure">
                     <img src="https://images2.listindiario.com/n/content/496/495697/p/680x460/201712201405381.jpeg" alt="Pet Image"/>
                 </figure>
-                <p className="Description">
-                Únete a AdoptaPet para apoyar su cuidado
-                y adopción. Dona para proporcionar refugio, alimento
-                y atención veterinaria. Cada donación ayuda a 
-                encontrarles hogares amorosos. Únete como donante 
-                regular para brindar estabilidad. Comparte nuestra 
-                página y genera un impacto positivo en las vidas de 
-                estas mascotas. Tu donación hace la diferencia. 
-                ¡Gracias por unirte a AdoptaPet
-                </p>
-                <footer className="footerDona">
-                    <a href="https://whas.me/YbmJSzxOs0" className="ButtonDona">Dona
-                        <img src="https://whas.me/media/_button_image" alt="Chat via WhatsApp"/>
-                    </a>
-                </footer>
-            </section>
-        </article>
+                <ul className="UlDescription">
+                    <li className="Description">
+                        Únete a AdoptaPet para apoyar su cuidado
+                        y adopción. Dona para proporcionar refugio, alimento
+                        y atención veterinaria. Cada donación ayuda a 
+                        encontrarles hogares amorosos. Únete como donante 
+                        regular para brindar estabilidad. Comparte nuestra 
+                        página y genera un impacto positivo en las vidas de 
+                        estas mascotas. Tu donación hace la diferencia. 
+                        ¡Gracias por unirte a AdoptaPet
+                    </li>
+                    <button className="ButtonDona" onClick={()=>{sendwhatsappDonate(dona)}}>Dona</button>
+                </ul>
+            </article>
+        </section>
     </>);
     };
 
