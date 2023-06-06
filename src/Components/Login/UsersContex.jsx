71684d9a181eea3bb55f26d2e75b1531115bec75
Cristian Betancourt <cristian.camilo.betancourt.rin@gmail.com>
2023-06-06 12:53:59 -0500

    Fix main

--- src/Components/Login/UsersContex.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const USERSCONTEXT = createContext([]); 
-
-export default USERSCONTEXT;
--- src/Pages/Info.jsx
+++
@@ -1,9 +0,0 @@
-import { KnowMore } from "../Components/KnowMore/KnowMore";
-import { Layout } from "../Components/Layout/Layout";
-export const Info = () => {
-  return (
-    <Layout>
-      <KnowMore/>
-    </Layout>
-  );
-};
